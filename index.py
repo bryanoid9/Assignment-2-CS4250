6b69492e870ad10d21a516cf7e2ad850121f0b01
bryanoid9 <90728547+bryanoid9@users.noreply.github.com>
2024-04-15 10:38:46 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: Bryan Martinez Ramirez
+# FILENAME: index.py
 # SPECIFICATION: description of the program
 # FOR: CS 4250- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT:6 houyrs
 #-----------------------------------------------------------*/
 
 #importing some Python libraries
